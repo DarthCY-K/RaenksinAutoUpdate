621c4c3e46ded58800905ce553fea694c81f92a8
DarthCY <452710557@qq.com>
2019-12-27 13:02:50 +0800

    增加检查版本和基本功能
    仅限客户端模拟

--- AutoUpdater-for-mcserver-mods.cpp
+++ AutoUpdater-for-mcserver-mods.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <io.h>
+#include <cstdio>
 #include <string>
 
 using namespace std;
@@ -12,20 +13,28 @@ typedef struct modsList
 	modsList* next = nullptr;
 };
 
-int updateLocal();
+typedef struct modSolutionList
+{
+	string mod_name;
+	int solution = 0;	//0为不存在 1为增加 2为删除
+	modSolutionList* next = nullptr;
+};
+
+int updateLocalInf();
+int checkVersion();
+int updateLocalMods();
+int updateLocalVersion(string new_version);
 
 int main()
 {
-	int choose = 0;
-	//printf("1.Update the local inf\t 2.Update the server inf\n");
-	//printf("Please enter the number:");
-	updateLocal();
+	updateLocalInf();
+	checkVersion();
 
 	getchar();
 	return 0;
 }
 
-int updateLocal()
+int updateLocalInf()
 {
 	string path = "C:\\Users\\45271\\Desktop\\TestProject\\AutoUpdater-for-mcserver-mods\\.minecraft\\mods\\";		//使用相对路径进行检索
 	string search_path = path + "*.jar";
@@ -74,7 +83,110 @@ int updateLocal()
 
 }
 
-int updateFile()
+int updateLocalVersion(string new_version)
 {
+	string path = "C:\\Users\\45271\\Desktop\\TestProject\\AutoUpdater-for-mcserver-mods\\.minecraft\\AutoUpdater\\version_client.xml";
 
+	ofstream file(path, ifstream::out);	//以清除并增加的方式打开文件
+	if (file.is_open())
+	{
+		file << new_version;
+		file.close();
+	}
+	return 0;
 }
+
+int checkVersion()	//用于比对文件版本号
+{
+	string server_version = "2.0";
+	string client_version;
+	char rua[100];
+	string path = "C:\\Users\\45271\\Desktop\\TestProject\\AutoUpdater-for-mcserver-mods\\.minecraft\\AutoUpdater\\version_client.xml";
+	
+	ifstream in(path, ifstream::in);	//以只读方式打开文件
+	if (in.is_open())
+	{
+		while (!in.eof())
+		{
+			//in >> client_version;
+			getline(in, client_version);
+		}
+		in.close();
+	}
+	cout << "client_version：V" << client_version << endl;
+	cout << "server_version：V" << server_version << endl;
+
+	if (client_version != server_version)
+	{
+		//ask for update
+		if (updateLocalMods() == 0)
+		{
+			if (updateLocalVersion(server_version) == 0)
+			{
+				client_version = server_version;
+				cout << "更新成功！新的本地版本号为V" + client_version << endl;
+			}
+
+		}
+	}
+	else
+		cout << "客户端已经是最新版本，无需更新" << endl;
+
+	return 0;
+}
+
+int updateLocalMods()
+{
+	//download solution_list from server
+	modSolutionList* head = new modSolutionList;
+	modSolutionList* temp_solution = new modSolutionList;
+	temp_solution = head;
+	string solution_path = "C:\\Users\\45271\\Desktop\\TestProject\\AutoUpdater-for-mcserver-mods\\.minecraft\\AutoUpdater\\Temp\\solution_list.xml";
+	string mods_path = "C:\\Users\\45271\\Desktop\\TestProject\\AutoUpdater-for-mcserver-mods\\.minecraft\\mods\\";
+	ifstream in(solution_path, ifstream::in);
+	if (in.is_open())
+	{
+		while (!in.eof())
+		{
+			in >> temp_solution->mod_name;
+			in >> temp_solution->solution;
+
+			temp_solution->next = new modSolutionList;
+			temp_solution = temp_solution->next;
+		}
+		in.close();
+	}
+
+	cout << "==================================================" << endl;
+	cout << "Mods Need To Be Modify" << endl;
+	cout << "Mod Name\t\t\tSolution" << endl;
+	temp_solution = head;
+	while ((temp_solution->next != nullptr) && (temp_solution->solution != 0))
+	{
+		cout << temp_solution->mod_name << "\t\t\t\t" << temp_solution->solution << endl;
+		temp_solution = temp_solution->next;
+	}
+
+	temp_solution = head;
+	while ((temp_solution->next != nullptr) && (temp_solution->solution != 0))
+	{
+		if (temp_solution->solution == 2)		//2为删除方案
+		{
+			if (remove((mods_path + temp_solution->mod_name).c_str()) == 0)
+				cout << "文件名：" << temp_solution->mod_name << "已删除" << endl;
+			else
+			{
+				cout << "文件名：" << temp_solution->mod_name << "删除失败，不存在" << endl;
+			}
+			temp_solution = temp_solution->next;
+		}
+		else
+		{
+			//get file from server
+		}
+	}
+
+	return 0;
+}
+
+
